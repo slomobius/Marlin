f81edc33bdd42f527772d77000f9a70eef61fa42
slomobius <59780209+slomobius@users.noreply.github.com>
2021-09-24 20:03:51 -0600

    Adjusted final settings before flashing

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1245,7 +1245,7 @@
   #endif
 
   // Insert a menu for preheating at the top level to allow for quick access
-  //#define PREHEAT_SHORTCUT_MENU_ITEM
+  #define PREHEAT_SHORTCUT_MENU_ITEM
 
 #endif // HAS_LCD_MENU
 
