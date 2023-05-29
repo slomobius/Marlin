0c98eda009f23e2f0240feb4458c97462bd9165c
slomobius <59780209+slomobius@users.noreply.github.com>
2023-05-29 15:29:18 -0600

    Adjust Z Offset & Esteps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1170,7 +1170,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 93 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 154 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
@@ -1485,7 +1485,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, -12, -2.66 }
+#define NOZZLE_TO_PROBE_OFFSET { -45, -12, -2.75 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
