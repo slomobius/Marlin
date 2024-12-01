41758c902601040f4b91aa7b2753a2e5637573eb
slomobius <59780209+slomobius@users.noreply.github.com>
2024-11-30 17:36:37 -0700

    Adjusting Tramming Points
    First Adjustment for Tramming Points.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1036,7 +1036,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define from 3 to 9 points to probe.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  27, 35 }, { 197,  35 }, { 197, 205 }, { 27, 205 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
