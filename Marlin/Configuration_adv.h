d56b929d92e496c59863d271f497a724a2baad5d
slomobius <59780209+slomobius@users.noreply.github.com>
2026-01-19 18:24:23 -0700

    Correct Bed Tramming Points

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1036,7 +1036,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define from 3 to 9 points to probe.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 214 }, { 20, 214 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
